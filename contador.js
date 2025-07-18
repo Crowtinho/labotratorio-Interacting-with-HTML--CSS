0f5d0a4f5ae6a966471cbed3d74b9cc35bf61e0a
Andres Cuervo <andres.cuervo0807@gmail.com>
2025-07-18 16:49:02 -0500

    added style

--- contador.js
+++ contador.js
@@ -4,10 +4,23 @@ let contador  = 0;
 document.getElementById("aumentar").addEventListener("click", function(){
     contador += 1;
     document.getElementById("contador").innerHTML = contador;
-
+    if(contador == 10){
+    document.getElementById("contador").style.color = "white"
+    document.getElementById("contador").style.background = "rgb(93, 173, 226)"
+    }else{
+    document.getElementById("contador").style.color = "black"
+    document.getElementById("contador").style.background = "transparent"
+    }
 }) 
 document.getElementById("disminuir").addEventListener("click", function(){
     contador -= 1;
     document.getElementById("contador").innerHTML = contador;
-
+    if(contador == 10){
+    document.getElementById("contador").style.color = "white"
+    document.getElementById("contador").style.background = "rgb(93, 173, 226)"
+    }else{
+    document.getElementById("contador").style.color = "black"
+    document.getElementById("contador").style.background = "transparent"
+    }
 }) 
+
